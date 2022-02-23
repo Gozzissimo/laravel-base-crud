d69f3d688a57f33631603d035d45d58005703ae9
Gozzissimo <fantini.nico@gmail.com>
2022-02-23 17:40:19 +0100

    Model completed

--- app/Comic.php
+++ app/Comic.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-    //
+    protected $fillable = [
+        'isbn',
+        'title',
+        'number',
+        'author',
+        'format',
+        'publisher',
+        'edition',
+        'language',
+        'pages',
+        'price',
+        'created_at',
+        'updated_at',
+    ];
 }

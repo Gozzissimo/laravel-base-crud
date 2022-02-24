bff15ff4df0789e526d6febbb9f7a8cb57a66f08
Gozzissimo <fantini.nico@gmail.com>
2022-02-24 17:46:26 +0100

    fixed route and id recall

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,7 +12,7 @@
             </div>
         </div>
         <div class="row">
-            <form action="{{ route('comics.store') }}" method="post">
+            <form action="{{ route('comics.update', ['id' => $comic->id]) }}" method="post">
                 @csrf
                 @method('PUT')
 
